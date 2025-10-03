87e04d89ca1fee1f5ae54f525131ca93819e9a98
Felipe Santana de Oliveira <f.b.de.oliveira@avanade.com>
2025-10-03 11:15:29 -0300

    feat/auth integration with database

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,20 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
+import prisma from "@/lib/prisma"
 
 const handler = NextAuth({
+    adapter: PrismaAdapter(prisma),
+
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
-    ]
+    ],
+    session: {
+        strategy: "jwt"
+    }
 })
 
 export { handler as GET, handler as POST}
